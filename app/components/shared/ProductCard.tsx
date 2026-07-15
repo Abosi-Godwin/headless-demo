dac881547064f1311b8aa3b81c8101007a7bbec4
Abosi-Godwin <bbnl6060@github.com>
2026-07-15 14:29:39 +0100

    Fifth  commit

--- app/components/shared/ProductCard.tsx
+++ app/components/shared/ProductCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ShopifyProduct } from "../../lib/types";
+import { ShopifyProduct } from "../../../lib/types";
 
 function getIssueNumber(id: string) {
     const digits = id.replace(/\D/g, "");
