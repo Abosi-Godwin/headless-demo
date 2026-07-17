9e4d14ce641a7081b74cc1fab4cbf576711f4c8c
Abosi-Godwin <bbnl6060@github.com>
2026-07-17 07:33:29 +0100

    use real shopify products

--- lib/Shopify.ts
+++ lib/Shopify.ts
@@ -1,4 +1,4 @@
-
+/*
 const endpoint = "https://mock.shop/api";
 
 export async function shopifyFetch<T>(
@@ -19,5 +19,27 @@ export async function shopifyFetch<T>(
         throw new Error("Failed to fetch Shopify API data");
     }
 
+    return json.data;
+}
+*/
+
+
+
+const domain = process.env.SHOPIFY_STORE_DOMAIN;
+const token = process.env.SHOPIFY_STOREFRONT_TOKEN;
+
+const endpoint = `https://${domain}/api/2026-04/graphql.json`;
+
+export async function shopifyFetch<T>(query: string, variables = {}): Promise<T> {
+    const res = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Shopify-Storefront-Private-Token': token!,
+        },
+        body: JSON.stringify({ query, variables }),
+        cache: 'no-store',
+    });
+    const json = await res.json();
     return json.data;
 }
